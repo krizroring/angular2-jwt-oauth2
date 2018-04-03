948431c4eded0fb643552a5e6633878d554f9a82
Christian Roring <c.roring@recognize.nl>
2018-04-03 10:29:13 +0200

    Added blacklisting domains

--- src/jwt.helper.ts
+++ src/jwt.helper.ts
@@ -1,74 +1,74 @@
 export class JwtHelper {
 
-  public static urlBase64Decode(str: string): string {
-    let output = str.replace(/-/g, '+').replace(/_/g, '/');
-    switch (output.length % 4) {
-      case 0: {
-        break;
-      }
-      case 2: {
-        output += '==';
-        break;
-      }
-      case 3: {
-        output += '=';
-        break;
-      }
-      default: {
-        throw 'Illegal base64url string!';
-      }
-    }
-    return this.b64DecodeUnicode(output);
-  }
-
-  public static decodeToken(token: string): object|null {
-    if(token === null || token === '') {
-      return null;
-    }
-    let parts = token.split('.');
-
-    if (parts.length !== 3) {
-      throw new Error('The inspected token doesn\'t appear to be a JWT. Check to make sure it has three parts and see https://jwt.io for more.');
+    public static urlBase64Decode(str: string): string {
+        let output = str.replace(/-/g, '+').replace(/_/g, '/');
+        switch (output.length % 4) {
+            case 0: {
+                break;
+            }
+            case 2: {
+                output += '==';
+                break;
+            }
+            case 3: {
+                output += '=';
+                break;
+            }
+            default: {
+                throw 'Illegal base64url string!';
+            }
+        }
+        return this.b64DecodeUnicode(output);
     }
 
-    let decoded = this.urlBase64Decode(parts[1]);
-    if (!decoded) {
-      throw new Error('Cannot decode the token.');
+    public static decodeToken(token: string): object | null {
+        if (token === null || token === '') {
+            return null;
+        }
+        let parts = token.split('.');
+
+        if (parts.length !== 3) {
+            throw new Error('The inspected token doesn\'t appear to be a JWT. Check to make sure it has three parts and see https://jwt.io for more.');
+        }
+
+        let decoded = this.urlBase64Decode(parts[1]);
+        if (!decoded) {
+            throw new Error('Cannot decode the token.');
+        }
+
+        return JSON.parse(decoded);
     }
 
-    return JSON.parse(decoded);
-  }
+    public static getTokenExpirationDate(token: string): Date | null {
+        let decoded: any;
+        decoded = this.decodeToken(token);
 
-  public static getTokenExpirationDate(token: string): Date|null {
-    let decoded: any;
-    decoded = this.decodeToken(token);
+        if (!decoded.hasOwnProperty('exp')) {
+            return null;
+        }
 
-    if (!decoded.hasOwnProperty('exp')) {
-      return null;
+        const date = new Date(0);
+        date.setUTCSeconds(decoded.exp);
+
+        return date;
     }
 
-    const date = new Date(0);
-    date.setUTCSeconds(decoded.exp);
+    public static isTokenExpired(token: string, offsetSeconds?: number): boolean {
+        let date = this.getTokenExpirationDate(token);
+        offsetSeconds = offsetSeconds || 0;
 
-    return date;
-  }
+        if (date === null) {
+            return true;
+        }
 
-  public static isTokenExpired(token: string, offsetSeconds?: number): boolean {
-    let date = this.getTokenExpirationDate(token);
-    offsetSeconds = offsetSeconds || 0;
-
-    if (date === null) {
-      return false;
+        return !(date.valueOf() > new Date().valueOf() + offsetSeconds * 1000);
     }
 
-    return !(date.valueOf() > new Date().valueOf() + offsetSeconds * 1000);
-  }
+    private static b64DecodeUnicode(str: any): string {
+        return decodeURIComponent(atob(str).split('').map(JwtHelper.percentEncodeChar).join(''));
+    }
 
-  private static b64DecodeUnicode(str: any): string {
-    return decodeURIComponent(atob(str).split('').map(JwtHelper.percentEncodeChar).join(''));
-  }
-
-  private static percentEncodeChar(char: string): string {
-    return '%' + ('00' + char.charCodeAt(0).toString(16)).slice(-2);
-  }
+    private static percentEncodeChar(char: string): string {
+        return '%' + ('00' + char.charCodeAt(0).toString(16)).slice(-2);
+    }
 }
--- src/jwt.interceptor.ts
+++ src/jwt.interceptor.ts
@@ -1,91 +1,104 @@
-import { Injectable, Inject } from '@angular/core';
+import {Injectable, Inject} from '@angular/core';
 import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
+    HttpRequest,
+    HttpHandler,
+    HttpEvent,
+    HttpInterceptor
 } from '@angular/common/http';
-import { JwtHelper } from './jwt.helper';
-import { JWT_OPTIONS } from './jwtoptions.token';
-import { Observable } from 'rxjs/Observable';
+import {JwtHelper} from './jwt.helper';
+import {JWT_OPTIONS} from './jwtoptions.token';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/mergeMap';
+const URL = require('url');
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  tokenGetter: () => string | Promise<string>;
-  headerName: string;
-  authScheme: string;
-  whitelistedDomains: Array<string | RegExp>;
-  throwNoTokenError: boolean;
-  skipWhenExpired: boolean;
+    tokenGetter: () => string | Promise<string>;
+    headerName: string;
+    authScheme: string;
+    whitelistedDomains: Array<string | RegExp>;
+    blacklistedRoutes: Array<string | RegExp>;
+    throwNoTokenError: boolean;
+    skipWhenExpired: boolean;
 
-  constructor(
-    @Inject(JWT_OPTIONS) config: any
-  ) {
-    this.tokenGetter = config.tokenGetter;
-    this.headerName = config.headerName || 'Authorization';
-    this.authScheme =
-      config.authScheme || config.authScheme === ''
-        ? config.authScheme
-        : 'Bearer ';
-    this.whitelistedDomains = config.whitelistedDomains || [];
-    this.throwNoTokenError = config.throwNoTokenError || false;
-    this.skipWhenExpired = config.skipWhenExpired;
-  }
-
-  isWhitelistedDomain(request: HttpRequest<any>): boolean {
-    const requestUrl = new URL(request.url);
-
-    return (
-      this.whitelistedDomains.findIndex(
-        domain =>
-          typeof domain === 'string'
-            ? domain === requestUrl.host
-            : domain instanceof RegExp ? domain.test(requestUrl.host) : false
-      ) > -1
-    );
-  }
-
-  handleInterception(
-    token: string,
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ) {
-    let tokenIsExpired: boolean = false;
-
-    if (!token && this.throwNoTokenError) {
-      throw new Error('Could not get token from tokenGetter function.');
+    constructor(@Inject(JWT_OPTIONS) config: any) {
+        this.tokenGetter = config.tokenGetter;
+        this.headerName = config.headerName || 'Authorization';
+        this.authScheme =
+            config.authScheme || config.authScheme === ''
+                ? config.authScheme
+                : 'Bearer ';
+        this.whitelistedDomains = config.whitelistedDomains || [];
+        this.blacklistedRoutes = config.blacklistedRoutes || [];
+        this.throwNoTokenError = config.throwNoTokenError || false;
+        this.skipWhenExpired = config.skipWhenExpired;
     }
 
-    if (this.skipWhenExpired) {
-      tokenIsExpired = token ? JwtHelper.isTokenExpired(token) : true;
+    isWhitelistedDomain(request: HttpRequest<any>): boolean {
+        const requestUrl = new URL(request.url);
+
+        return (
+            this.whitelistedDomains.findIndex(
+                domain =>
+                    typeof domain === 'string'
+                        ? domain === requestUrl.host
+                        : domain instanceof RegExp ? domain.test(requestUrl.host) : false
+            ) > -1
+        );
     }
 
-    if (token && tokenIsExpired && this.skipWhenExpired) {
-      request = request.clone();
-    } else if (token && this.isWhitelistedDomain(request)) {
-      request = request.clone({
-        setHeaders: {
-          [this.headerName]: `${this.authScheme}${token}`
+    isBlacklistedRoute(request: HttpRequest<any>): boolean {
+        const url = request.url;
+
+        return (
+            this.blacklistedRoutes.findIndex(
+                route =>
+                    typeof route === 'string'
+                        ? route === url
+                        : route instanceof RegExp ? route.test(url) : false
+            ) > -1
+        );
+    }
+
+    handleInterception(token: string, request: HttpRequest<any>, next: HttpHandler ) {
+        let tokenIsExpired: boolean = true;
+
+        // exit early
+        if (this.isBlacklistedRoute(request)) {
+            return next.handle(request);
         }
-      });
-    }
-    return next.handle(request);
-  }
 
-  intercept(
-    request: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    const token: any = this.tokenGetter();
+        if (!token && this.throwNoTokenError) {
+            throw new Error('Could not get token from tokenGetter function.');
+        }
 
-    if (token instanceof Promise) {
-      return Observable.fromPromise(token).mergeMap((asyncToken: string) => {
-        return this.handleInterception(asyncToken, request, next);
-      });
-    } else {
-      return this.handleInterception(token, request, next);
+        if (this.skipWhenExpired) {
+            tokenIsExpired = token ? JwtHelper.isTokenExpired(token) : true;
+        }
+
+        if (token && tokenIsExpired && this.skipWhenExpired) {
+            request = request.clone();
+        } else if (token && this.isWhitelistedDomain(request) && !this.isBlacklistedRoute(request)) {
+            request = request.clone({
+                setHeaders: {
+                    [this.headerName]: `${this.authScheme}${token}`
+                }
+            });
+        }
+        return next.handle(request);
     }
-  }
+
+    intercept(request: HttpRequest<any>,
+              next: HttpHandler): Observable<HttpEvent<any>> {
+        const token: any = this.tokenGetter();
+
+        if (token instanceof Promise) {
+            return Observable.fromPromise(token).mergeMap((asyncToken: string) => {
+                return this.handleInterception(asyncToken, request, next);
+            });
+        } else {
+            return this.handleInterception(token, request, next);
+        }
+    }
 }
